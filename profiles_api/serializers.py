27cef8c774995be6ef76502b0264caf619fef121
Vicente Andrés Muñoz Moller <Vicente@Vicentes-MBP.fibertel.com.ar>
2022-04-07 17:08:16 -0300

    Added UserProfileViewset and set up of permissions

--- profiles_api/serializers.py
+++ profiles_api/serializers.py
@@ -21,7 +21,7 @@ class UserProfileSerializer(serializers.ModelSerializer):
     
     def create(self, validated_data):
         """Create and return a new user"""
-        user = models.UserProfile.object.create_user(
+        user = models.UserProfile.objects.create_user(
             email=validated_data["email"],
             name=validated_data["name"],
             password=validated_data["password"],
